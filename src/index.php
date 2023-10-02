72725b252d098460c0006ad85ccf5eeda0e202ad
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2023-10-02 12:34:50 -0300

    Add - chama funcao recursiva

--- src/index.php
+++ src/index.php
@@ -1,17 +1,22 @@
 <?php
 
 /**
+ * Reverte uma string
+ * Reverte uma string usando função recursiva
+ *
+ * @author Tiago Farias <tiago.farias.poa@gmail.com>
  * 
+ * @string $word
  */
-function reverteString(string $word) : string
+function reverteString(string $word)
 {
     $word = trim($word);
-    if( empty($word) )
-    {
-        throw new \Exception('String vazia!');
-    }
 
-    return '';
+    if( !empty($word) )
+    {
+        echo ($word[strlen($word) - 1]);
+        reverteString(substr($word, 0,(strlen($word) - 1)));
+    }
 }
 
-echo reverteString('   ');
+echo reverteString('Tiago Oliveira de Farias');
